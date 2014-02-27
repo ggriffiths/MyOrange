f8ae5a59fe9e4d957195bcf54d8040962b26d204
Grant Griffiths <gwg@ieee.org>
2014-02-26 22:39:08 -0500

    Minor layout changes and linked index to main.

---
+++ client/js/app.js
@@ -0,0 +1,19 @@
+'use strict';
+
+angular.module('myApp', ['google-maps']);
+
+// Declare app level module which depends on filters, and services
+angular.module('myApp', [
+  'ngRoute',
+  'myApp.filters',
+  'myApp.services',
+  'myApp.directives',
+  'myApp.controllers'
+]).
+config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/schedule', {templateUrl: 'modules/schedule/index.html', controller: 'NavCtrl'});
+  $routeProvider.when('/map', {templateUrl: 'modules/map/index.html', controller: 'NavCtrl'});
+  $routeProvider.when('/communicator', {templateUrl: 'modules/communicator/index.html', controller: 'NavCtrl'});
+  $routeProvider.when('/degree', {templateUrl: 'modules/degree/index.html', controller: 'NavCtrl'});
+  $routeProvider.otherwise({redirectTo: '/schedule'});
+}]);
